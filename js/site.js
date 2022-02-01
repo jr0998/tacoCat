b07863b385b03c0a7c7c7ae3cf6f9e77a611a2c1
jr0998 <33519065+jr0998@users.noreply.github.com>
2022-02-01 12:31:59 -0500

    modified js with better function allocations

--- js/site.js
+++ js/site.js
@@ -1,31 +1,41 @@
 function getString() {
     document.getElementById("alert").classList.add("invisible");
-
+    let testStr = [];
     let str = document.getElementById("stringInput").value;
+    testStr = reverseStr(str);
+    str = composeString(str);
+    testStr = composeString(testStr);
+    let check = checkPal(str, testStr);
 
-    str = checkPal(str);
-
-    displayMessage(str);
+    displayMessage(check, str, testStr);
 
 }
 
-function checkPal(str) {
+function reverseStr(str) {
+    let testStr = [];
+    for (let i = str.length - 1; i >= 0; i--) {
+        testStr += str[i];
+    }
+    return testStr;
+}
+
+function composeString(str) {
     str = str.replace(/\s/g, '');
     return str;
 }
 
-function displayMessage(str) {
-    let testStr = [];
+function checkPal(str, testStr) {
     let check = true;
-    for (let i = str.length - 1; i >= 0; i--) {
-        testStr += str[i];
-    }
 
     for (let i = 0; i < str.length; i++) {
-        if (str[i].toLowerCase() != testStr[i].toLowerCase()) {
+        if (str[i].toLowerCase != testStr[i].toLowerCase) {
             check = false;
         }
     }
+    return check;
+}
+
+function displayMessage(check, str, testStr) {
     if (!check) {
         document.getElementById("alert").classList.remove("alert-success");
         document.getElementById("alert").classList.add("alert-danger");
